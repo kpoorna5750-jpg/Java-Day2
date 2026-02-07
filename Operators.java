9ecce84f2b8e982580d36696de5579678816848d
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-07 14:16:06 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,16 @@
+class Operators{
+    public static void main(String[] args){
+        int a=45;
+        int b=56;
+        int sum=a+b;
+        int diff=a-b;
+        int product=a*b;
+        int division=a%b;
+        int floor=a/b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(product);
+        System.out.println(division);
+        System.out.println(floor);
+    }
+}
