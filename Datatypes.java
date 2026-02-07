b493cd53a70a698124a1d51c9e92a6a438c56c46
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-07 13:33:57 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,13 @@
+class Datatypes{
+    public static void main(String[] args){
+        String MovieName="Mouse";
+        int seatNumber=0xA4;
+        int Price=100;
+        String status=" Ticket Booked";
+        System.out.println(MovieName);
+        System.out.println(seatNumber);
+        System.out.println(Price);
+        System.out.println(status);
+        
+    }
+}
